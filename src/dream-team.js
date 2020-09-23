a12c8d6a28ecf40948b462f7f3b2501ab9bc3646
Anatoliy <70736455+anatkig@users.noreply.github.com>
2020-09-23 22:59:19 +0600

    feat

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,7 +2,7 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(arr) {
   //throw new CustomError('Not implemented');
-  console.log(arr);
+ 
   if(Array.isArray(arr)&&arr.length>0)
  return arr.filter(a=>typeof a==="string").map(a=>a.trim()[0].toUpperCase()).sort().reduce((sum,i)=>sum+=i);
   else return false;
