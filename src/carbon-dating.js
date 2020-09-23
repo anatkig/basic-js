01cf761a330979975e492b890e7be818ef0581b7
Anatoliy <70736455+anatkig@users.noreply.github.com>
2020-09-23 12:12:39 +0600

    fix

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,7 +4,7 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-if(isNaN(sampleActivity)||sampleActivity==false||sampleActivity[0].toUpperCase()===sampleActivity[0].toLowerCase())
+if(isNaN(sampleActivity)||sampleActivity===undefined||sampleActivity==false||sampleActivity[0].toUpperCase()===sampleActivity[0].toLowerCase())
   return false;
   else return Math.ceil(Math.log(MODERN_ACTIVITY/HALF_LIFE_PERIOD)/parseInt(sampleActivity));
 };
