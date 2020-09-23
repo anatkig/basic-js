5e3984f31727ffb33f83293eaf399ef729a46662
Anatoliy <70736455+anatkig@users.noreply.github.com>
2020-09-23 22:10:07 +0600

    fix

--- src/count-cats.js
+++ src/count-cats.js
@@ -2,15 +2,14 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(number) {
   //throw new CustomError('Not implemented');
- /*let count=0;
+ let count=0;
   for(let i of number)
     for(let y of i)
       if(y ==='^^')
         count++;
   
-  return count;*/
+  return count;
   
-  return number.reduce((sum,i)=>sum.concat(i)).filter(a=>a==='^^').length;
-  
+
 
 };
