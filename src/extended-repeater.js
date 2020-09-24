1735b33bf9090865766575aab938ce6dd2ec59cd
Anatoliy <70736455+anatkig@users.noreply.github.com>
2020-09-24 09:38:56 +0600

    fix

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -3,24 +3,24 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function repeater(str,opt) {
  
 
- console.log(opt['repeatTimes']);
+
  
 // throw new CustomError('Not implemented');
- /*
- console.log(opt);
-  let options = opt;
+
+
+
   let st="";
-  if(!("repeatTimes" in options) options.repeatTimes=0;
-     if(!("separator" in options) options["separator"]="";
-  if(!("addition" in options) options["addition"]="";
-     if(!("additionRepeatTimes" in options) options["additionRepeatTimes"]=0;
-  if(!("additionSeparator" in options) options["additionSeparator"]="";
+  if(!('repeatTimes' in opt) opt['repeatTimes']=0;
+     if(!("separator" in opt) opt["separator"]="";
+  if(!("addition" in opt) opt["addition"]="";
+     if(!("additionRepeatTimes" in opt) opt["additionRepeatTimes"]=0;
+  if(!("additionSeparator" in opt) opt["additionSeparator"]="";
      
-     for(let i=0; i<options.repeatTimes; i++){
-    for(let y=0; y<options.additionRepeatTimes;y++)
-      st+=options.additionSeparator+options.addition;
-    st+=options.separator;
+     for(let i=0; i<opt['repeatTimes']; i++){
+    for(let y=0; y<opt.additionRepeatTimes;y++)
+      st+=opt.additionSeparator+opt.addition;
+    st+=opt.separator;
   }
-     return st;*/
+     return st;
 };
   
