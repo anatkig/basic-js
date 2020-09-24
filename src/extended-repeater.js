9e285d6d2735027700c1dc93ef2a662591554374
Anatoliy <70736455+anatkig@users.noreply.github.com>
2020-09-24 09:40:17 +0600

    fix

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -10,11 +10,11 @@ module.exports = function repeater(str,opt) {
 
 
   let st="";
-  if(!('repeatTimes' in opt) opt['repeatTimes']=0;
-     if(!("separator" in opt) opt["separator"]="";
-  if(!("addition" in opt) opt["addition"]="";
-     if(!("additionRepeatTimes" in opt) opt["additionRepeatTimes"]=0;
-  if(!("additionSeparator" in opt) opt["additionSeparator"]="";
+  if(!('repeatTimes' in opt)) opt['repeatTimes']=0;
+     if(!("separator" in opt)) opt["separator"]="";
+  if(!("addition" in opt)) opt["addition"]="";
+     if(!("additionRepeatTimes" in opt)) opt["additionRepeatTimes"]=0;
+  if(!("additionSeparator" in opt)) opt["additionSeparator"]="";
      
      for(let i=0; i<opt['repeatTimes']; i++){
     for(let y=0; y<opt.additionRepeatTimes;y++)
