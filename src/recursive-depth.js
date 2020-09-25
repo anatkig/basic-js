480797d7d3770bc50245881c07ff46a6b4432304
Anatoliy <70736455+anatkig@users.noreply.github.com>
2020-09-25 12:45:48 +0600

    fix

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -5,6 +5,7 @@ module.exports = class DepthCalculator {
     //throw new CustomError('Not implemented');
    console.log(arr);
     if(!Array.isArray(arr))return 0;
+    else if(arr.length===0)return 1;
     
     else return this.calculateDepth(arr.filter(a=>Array.isArray(a))[0])+1;
   }
