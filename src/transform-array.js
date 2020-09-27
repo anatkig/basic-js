f8d40dfc6b3a713c1531ef704e676ff64de085ea
Anatoliy <70736455+anatkig@users.noreply.github.com>
2020-09-27 22:29:48 +0600

    fix

--- src/transform-array.js
+++ src/transform-array.js
@@ -9,5 +9,17 @@ module.exports = function transform(arr) {
   const doubn="--double-next";
   const doubp="--double-prev";
   
+  let com=arr.filter(a=>typeof a==="string");
+  let arr2=new Array(arr);
+  
+ for(let i=0;i<arr2.length;i++){
+   if(arr2[i]==="--discard-next"){ arr2.splice(i+1,1); return arr2.filter(a=>Number.isInteger(a))}
+   if(arr2[i]==="--discard-prev"){ arr2.splice(i-1,1); return arr2.filter(a=>Number.isInteger(a))}
+   if(arr2[i]==="--double-next"){ arr2.splice(i+1,0,arr2[i+1]); return arr2.filter(a=>Number.isInteger(a))}
+   if(arr2[i]==="--double-prev"){ arr2.splice(i-1,0,arr2[i-1]); return arr2.filter(a=>Number.isInteger(a))}
+  
+ }
+  }
+  
   
 };
