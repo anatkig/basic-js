f70ab86f61c9a29ccb970a6471893fa2fc5f27df
Anatoliy <70736455+anatkig@users.noreply.github.com>
2020-09-27 10:12:44 +0600

    fix

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,42 +4,42 @@ const CustomError = require("../extensions/custom-error");
       
  const chainMaker =  {
   
-chain: "",
+
  arval: [],
   getLength() {
    
   
    // throw new CustomError('Not implemented');
-    return this.chain.length; 
+    return this.arval.length; 
   },
   addLink(value) {
-    this.chain+=value;
+ 
     this.arval.push(value);
      
   if(!value){
-   this.chain+="( )";
+   
     this.arval.push("( )");
   }
         return this;
   },
   removeLink(position) {
     //throw new CustomError('Not implemented');
-   this.chain=this.chain.replace(this.arval[position],"");
+ this.arval.splice(position,1);
      
         return this;
   },
   reverseChain() {
     //throw new CustomError('Not implemented');
         this.arval.reverse();
-    this.chain=this.arval.join("");
+  
         return this;
   },
   finishChain() {
     //throw new CustomError('Not implemented');
     
-    let ch=this.chain;
-    this.chain="";
-    return ch;
+  
+  
+    return this.arval.join("~~")
   }
 };
 
