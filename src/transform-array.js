54a06e31a26b3a96ac17e9f0509a451175e75229
Anatoliy <70736455+anatkig@users.noreply.github.com>
2020-09-27 22:36:42 +0600

    fix

--- src/transform-array.js
+++ src/transform-array.js
@@ -11,7 +11,7 @@ module.exports = function transform(arr) {
   
   let com=arr.filter(a=>typeof a==="string");
   let arr2=new Array(arr);
-  
+  console.log(arr2);
  for(let i=0;i<arr2.length;i++){
    if(arr2[i]==="--discard-next"){ arr2.splice(i+1,1); return arr2.filter(a=>Number.isInteger(a))}
    if(arr2[i]==="--discard-prev"){ arr2.splice(i-1,1); return arr2.filter(a=>Number.isInteger(a))}
