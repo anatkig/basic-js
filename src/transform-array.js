cf7b7c8e44979f3bcbaf91b85ab290eecbc53138
Anatoliy <70736455+anatkig@users.noreply.github.com>
2020-09-28 23:49:39 +0600

    fix

--- src/transform-array.js
+++ src/transform-array.js
@@ -20,17 +20,17 @@ module.exports = function transform(arr) {
       
       for(let y=0;y<arr2[i].length;y++){
      
-     if(arr2[i][y]==="--discard-next") arr2[i].splice(y,1);
-  else  if(arr2[i][y]==="--discard-prev"){ if(y-1===-1)/*arr2[i].splice(y,0);  else*/ arr2[i].splice(y-1,1)}
-   else if(arr2[i][y]==="--double-next"){if(y+1===arr2[i].length)/*arr2[i].splice(y,0);  else */arr2[i].splice(y,0,arr2[i][y+1])}
-   else if(arr2[i][y]==="--double-prev"){if(y-1===-1)/*arr2[i].splice(y,0);  else */arr2[i].splice(y,0,arr2[i][y-1])}
+     if(arr2[i][y]==="--discard-next") arr2[i].splice(y,2);
+  else  if(arr2[i][y]==="--discard-prev"){ if(y-1===-1)arr2[i].splice(y,1);  else arr2[i].splice(y-1,2)}
+   else if(arr2[i][y]==="--double-next"){if(y+1===arr2[i].length)arr2[i].splice(y,1);  else arr2[i].splice(y,1,arr2[i][y+1])}
+   else if(arr2[i][y]==="--double-prev"){if(y-1===-1)arr2[i].splice(y,1);  else arr2[i].splice(y,1,arr2[i][y-1])}
       }
    }
    else{
-   if(arr2[i]==="--discard-next"){ arr2.splice(i,1) }
-   else if(arr2[i]==="--discard-prev"){if(i-1===-1)/*arr2.splice(i,0);  else*/ arr2.splice(i-1,1)}
-   else if(arr2[i]==="--double-next"){ if(i+1===arr2.length)/* arr2.splice(i,0); else*/ arr2.splice(i,0,arr2[i+1])}
-   else if(arr2[i]==="--double-prev"){if(i-1===-1)/*arr2.splice(i,0);  else*/ arr2.splice(i,0,arr2[i-1])}
+   if(arr2[i]==="--discard-next"){ arr2.splice(i,2) }
+   else if(arr2[i]==="--discard-prev"){if(i-1===-1)arr2.splice(i,1);  else arr2.splice(i-1,2)}
+   else if(arr2[i]==="--double-next"){ if(i+1===arr2.length) arr2.splice(i,1); else arr2.splice(i,1,arr2[i+1])}
+   else if(arr2[i]==="--double-prev"){if(i-1===-1)arr2.splice(i,1);  else arr2.splice(i,1,arr2[i-1])}
    }
  
    }
