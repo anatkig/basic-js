d9306b27a851d2b6ea882cde751a82476d8cad7c
Anatoliy <70736455+anatkig@users.noreply.github.com>
2020-09-28 22:38:31 +0600

    fix

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,7 +14,7 @@ module.exports = function transform(arr) {
   if(arr.length===0)return arr;
   let arr2=new Array(arr);
 
- for(let i=0;i<arr2.length;i++)
+ for(let i=0;i<arr2.length;i++){
   
    if(Array.isArray(arr2[i])){
       
@@ -24,15 +24,15 @@ module.exports = function transform(arr) {
   else  if(arr2[i][y]==="--discard-prev"){ if(y-1===-1)arr2[i].splice(y,1);  else arr2[i].splice(y-1,2)}
    else if(arr2[i][y]==="--double-next"){if(y+1===arr2[i].length)arr2[i].splice(y,1);  else arr2[i].splice(y,1,arr2[i][y+1])}
    else if(arr2[i][y]==="--double-prev"){if(y-1===-1)arr2[i].splice(y,1);  else arr2[i].splice(y,1,arr2[i][y-1])}
- 
-   
+      }
+   }
    else{
    if(arr2[i]==="--discard-next"){ arr2.splice(i,2) }
    else if(arr2[i]==="--discard-prev"){if(i-1===-1)arr2.splice(i,1);  else arr2.splice(i-1,2)}
    else if(arr2[i]==="--double-next"){ if(i+1===arr2.length) arr2.splice(i,1); else arr2.splice(i,1,arr2[i+1])}
    else if(arr2[i]==="--double-prev"){if(i-1===-1)arr2.splice(i,1);  else arr2.splice(i,1,arr2[i-1])}
    }
- }
+ 
    }
   return arr2;
   
