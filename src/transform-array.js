bf2fd79c9b14d32f413ab5b9cf9c6f9470d56811
Anatoliy <70736455+anatkig@users.noreply.github.com>
2020-09-28 23:38:15 +0600

    fix

--- src/transform-array.js
+++ src/transform-array.js
@@ -21,7 +21,7 @@ module.exports = function transform(arr) {
       for(let y=0;y<arr2[i].length;y++){
      
      if(arr2[i][y]==="--discard-next") arr2[i].splice(y,1);
-  else  if(arr2[i][y]==="--discard-prev"){ if(y-1===-1)arr2[i].splice(y,1);  else arr2[i].splice(y-1,1)}
+  else  if(arr2[i][y]==="--discard-prev"){ if(y-1===-1)arr2[i].splice(y,0);  else arr2[i].splice(y-1,1)}
    else if(arr2[i][y]==="--double-next"){if(y+1===arr2[i].length)arr2[i].splice(y,0);  else arr2[i].splice(y,0,arr2[i][y+1])}
    else if(arr2[i][y]==="--double-prev"){if(y-1===-1)arr2[i].splice(y,0);  else arr2[i].splice(y,0,arr2[i][y-1])}
       }
