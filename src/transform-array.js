3bcfd9f481ac6f40a2028e03814179ef041edd54
Anatoliy <70736455+anatkig@users.noreply.github.com>
2020-09-28 11:20:05 +0600

    fix

--- src/transform-array.js
+++ src/transform-array.js
@@ -21,16 +21,16 @@ module.exports = function transform(arr) {
       for(let y=0;y<arr2[i].length;y++){
         console.log(Array.isArray(arr2[i][y])+"prov")
      if(arr2[i][y]==="--discard-next"){ arr2[i].splice(y,2) };
-   if(arr2[i][y]==="--discard-prev"){ arr2[i].splice(y-1,2)};
-   if(arr2[i][y]==="--double-next"){ arr2[i].splice(y,1,arr2[y+1])};
-   if(arr2[i][y]==="--double-prev"){ arr2[i].splice(y,1,arr2[y-1])};
+   if(arr2[i][y]==="--discard-prev"){ if(y-1===-1)arr2[i].splice(y,1)  else arr2[y].splice(i-1,2)};
+   if(arr2[i][y]==="--double-next"){if(y+1===arr2[i].length)arr2[i].splice(y,1)  else arr2[i].splice(y,1,arr2[y+1])};
+   if(arr2[i][y]==="--double-prev"){if(y-1===-1)arr2[i].splice(y,1)  arr2[i].splice(y,1,arr2[y-1])};
    
       }  
    else{
    if(arr2[i]==="--discard-next"){ arr2.splice(i,2) };
-   if(arr2[i]==="--discard-prev"){ arr2.splice(i-1,2)};
-   if(arr2[i]==="--double-next"){ arr2.splice(i,1,arr2[i+1])};
-   if(arr2[i]==="--double-prev"){ arr2.splice(i,1,arr2[i-1])};
+   if(arr2[i]==="--discard-prev"){if(i-1===-1)arr2.splice(i,1)  else arr2.splice(i-1,2)};
+   if(arr2[i]==="--double-next"){ if(i+1===arr2.length) arr2.splice(i,1) else arr2.splice(i,1,arr2[i+1])};
+   if(arr2[i]==="--double-prev"){if(i-1===-1)arr2.splice(i,1)  else arr2.splice(i,1,arr2[i-1])};
    }
  }
   return arr2;
