0f2f6c4f2b70bd08ac663eb6fd76a6b4f1cec17a
Anatoliy <70736455+anatkig@users.noreply.github.com>
2020-09-28 22:52:01 +0600

    fix

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,7 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
- 
+ console.log(arr)
   if(!Array.isArray(arr))
   throw new CustomError('trash');
   
