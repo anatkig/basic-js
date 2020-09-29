ffba2b7f1bfd14cb3db518b59185fe43bd5e2e8a
Anatoliy <70736455+anatkig@users.noreply.github.com>
2020-09-29 13:11:15 +0600

    fix

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,7 +20,7 @@ class VigenereCipheringMachine {
       let diff=arr[i].charCodeAt(0)-97;
         let keyc=keyl.charCodeAt((i-count)%keyl.length);
      
-        let index=(keyc+diff)<=122?(keyc+diff):((keyc+diff)%122)+97;
+        let index=(keyc+diff)<=122?(keyc+diff):((keyc+diff)%122)+96;
            console.log(index);
         let newlet=String.fromCharCode(index);
       arr[i]=newlet;
