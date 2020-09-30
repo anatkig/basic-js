fe379645110ee717df3e6b61c130ec31efc3a759
Anatoliy <70736455+anatkig@users.noreply.github.com>
2020-09-30 23:22:08 +0600

    fix

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -29,7 +29,7 @@ const CustomError = require("../extensions/custom-error");
     console.log(position +"pos")
         if(typeof position!=="number"||isNaN(position)||!Number.isInteger(position)||position>this.arval.length||position<1){
               this.arval.splice(0,this.arval.length);
-              console.log(this.arval+"arval")
+             
     throw new Error('THROWN');
         }
         else
